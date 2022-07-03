54f17a83f2207f660436bcf4effc595cf1e503f3
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-07-03 14:42:28 -0300

    Componente: Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -110,8 +111,9 @@ export default function UserList() {
                         </Tbody>
                     </Table>
 
-                </Box>
+                    <Pagination />
 
+                </Box>
             </Flex>
         </Box>
     );
